f7ad40fae608918f2816537f4200cf7c3c411bfa
betopicanco <betopicanco@gmail.com>
2022-06-15 12:43:42 -0300

    start project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className={` text-blue-600 `}>
+      Hello World!!!
+    </div>
+  )
+}
+
+export default Home
